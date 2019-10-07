b38726878d975d5c25ee9c3305997e6ad2faebad
Manoj2210 <manojkumarmaths1000@gmail.com>
2019-10-07 18:30:03 +0530

    Few changes in getting input

--- Tree.py
+++ Tree.py
@@ -77,7 +77,7 @@ class KD:
         return math.sqrt(abs(dist))
     
     currentBest=0
-    nearestDistance=100000
+    nearestDistance=1000000000000000
 
     def nearestNeighbour(self,root,color):
         if root==None:
@@ -96,13 +96,13 @@ class KD:
             left_distance=self.distance(root.left,color)
         
         else :
-            left_distance=100000
+            left_distance=100000000000000000
 
         if root.right!=None:
             right_distance=self.distance(root.right,color)
         
         else:
-            right_distance=100000
+            right_distance=100000000000000000
 
         if left_distance < right_distance:
             minChildDistance=left_distance
@@ -110,6 +110,8 @@ class KD:
         else:
             minChildDistance=right_distance
             currentBest=root.right
+
+        print(root_distance,left_distance,right_distance)
 
         if root_distance>minChildDistance:
             if(self.nearestDistance>minChildDistance):   
@@ -122,36 +124,67 @@ class KD:
                 self.nearestDistance=root_distance
             self.nearestNeighbour(currentBest,color)
 
-  
+    def nearestNeighbourfunction(self,color):
+        self.nearestNeighbour(self.root,color)
+        # print(self.currentBest.rgb_value)
+        return self.currentBest.rgb_value
+
+from PIL import Image
+
 if __name__ == "__main__":
-   
-    t = KD((5,1,1))
-
-    points= [ (9, 3 ,6),(7, 9 ,8),(1, 7 ,3),(3, 8 ,9),(6, 5 ,1),(8, 3 ,6) ]
-    
-    no_elts=6
-    
-    for i in range(no_elts):
+    t=KD([0,0,0]) #black
+    import csv
+    points=[]
+    with open('CSS140.txt','r')as f:
+        data = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+        for row in data:
+            row=[int(x) for x in row]
+            points.append(row)
+    for i in range(len(points)):
         t.root= t.insert(t.root,points[i])
-        
     t.LevelOrder(t.root)
+    # t.nearestNeighbour(t.root,(213,85,10))
 
-    t.nearestNeighbour(t.root,(10,4,3))
+    # print(t.currentBest.rgb_value)
+
+    # im=Image.open('index.jpeg')
+    # pix = im.load()
+
+    # for x in range(im.size[0]):
+    #     for y in range(im.size[1]):
+    #         m=pix[x,y]
+    #         pix[x,y]=t.nearestNeighbourfunction(m)
+
+    # im.save('changed.png')
+
+
+#     t = KD((5,1,1))
+
+#     points= [ (9, 3 ,6),(7, 9 ,8),(1, 7 ,3),(3, 8 ,9),(6, 5 ,1),(8, 3 ,6) ]
     
-    m=t.currentBest.rgb_value
-
-    print("Nearest of (10,4,3) is "+ str(m))
-
-    p1= (9,3,6) 
-    p2= (2,2,1) 
+#     no_elts=6
     
-    test=[p1,p2]
-    for i in test:
-        ans=t.Search(t.root,i)
-        if(ans==True):
-            print("FOUND")
-        else:
-            print("NOT FOUND")
+#     for i in range(no_elts):
+#         t.root= t.insert(t.root,points[i])
+        
+#     t.LevelOrder(t.root)
+
+#     t.nearestNeighbour(t.root,(10,4,3))
+    
+#     m=t.currentBest.rgb_value
+
+#     print("Nearest of (10,4,3) is "+ str(m))
+
+#     p1= (9,3,6) 
+#     p2= (2,2,1) 
+    
+#     test=[p1,p2]
+#     for i in test:
+#         ans=t.Search(t.root,i)
+#         if(ans==True):
+#             print("FOUND")
+#         else:
+#             print("NOT FOUND")
     
     
     
